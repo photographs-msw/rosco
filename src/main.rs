782e675b974b9c79f03f219007f1465b13a3e9d1
photographs-msw <rubles.canto0l@icloud.com>
2024-07-14 17:39:34 -0400

    Moved stream creation to macro. Added support for triangle, saw, and square wave oscillators

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,8 @@ extern crate cpal;
 use std::env;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
-// static NUM_ARGS: usize = 3;
 static SAMPLE_RATE: f32 = 44100.0;
 static TWO_PI: f32 = 2.0 * std::f32::consts::PI;
-// static FREQUENCY: f32 = 440.0;
 
 fn main() {
     let args = get_args();
@@ -19,39 +17,24 @@ fn main() {
 
     match osc_type.as_str() {
         "sine" => run_sin_gen::<f32>(&device, &config.into(), frequency),
+        "triangle" => run_triangle_gen::<f32>(&device, &config.into(), frequency),
+        "square" => run_square_gen::<f32>(&device, &config.into(), frequency),
+        "saw" => run_saw_gen::<f32>(&device, &config.into(), frequency),
         _ => run_sin_gen::<f32>(&device, &config.into(), frequency),
     }
-    // run_sin_gen::<f32>(&device, &config.into(), frequency);
 }
 
-fn run_sin_gen<T>(device: &cpal::Device, config: &cpal::StreamConfig, frequency: f32)
-where
-    T: cpal::Sample + cpal::SizedSample + cpal::FromSample<f32>,
-{
-    // Produce a sinusoid of maximum amplitude.
-    // let sample_rate = 44100.0;
-    let mut sample_clock = 0f32;
-    let mut next_value = move || {
-        sample_clock = (sample_clock + 1.0) % SAMPLE_RATE;
-        (sample_clock * frequency * TWO_PI / SAMPLE_RATE).sin()
+macro_rules! create_stream {
+    ($device:expr, $config:expr, $channels:expr, $err_fn:expr, $next_value:expr) => {
+        $device.build_output_stream(
+            $config,
+            move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+                write_data::<T>(data, $channels, $next_value)
+            },
+            $err_fn,
+            None,
+        ).unwrap()
     };
-
-    let channels = config.channels as usize;
-    let err_fn =
-        |err| eprintln!("an error occurred on the output audio stream: {}", err);
-
-    let stream =
-        device.build_output_stream(config,
-                                   move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
-                                       write_data::<T>(data, channels, &mut next_value)
-                                   },
-                                   err_fn,
-                                   None)
-            .unwrap();
-    stream.play().unwrap();
-
-    // Keep the stream alive indefinitely to play sound
-    std::thread::sleep(std::time::Duration::from_millis(10000));
 }
 
 fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
@@ -70,4 +53,94 @@ where
 fn get_args() -> Vec<String> {
     let args: Vec<String> = env::args().skip(1).collect();
     return args;
+}
+
+fn run_sin_gen<T>(device: &cpal::Device, config: &cpal::StreamConfig, frequency: f32)
+where
+    T: cpal::Sample + cpal::SizedSample + cpal::FromSample<f32>,
+{
+    let mut sample_clock = 0f32;
+    // Produce a sinusoid of maximum amplitude.
+    let mut next_value = move || {
+        sample_clock = (sample_clock + 1.0) % SAMPLE_RATE;
+        (sample_clock * frequency * TWO_PI / SAMPLE_RATE).sin()
+    };
+
+    let channels = config.channels as usize;
+    let err_fn =
+        |err| eprintln!("an error occurred on the output audio stream: {}", err);
+    let stream = create_stream!(device, config, channels, err_fn, &mut next_value);
+    stream.play().unwrap();
+    // Keep the stream alive indefinitely to play sound
+    std::thread::sleep(std::time::Duration::from_millis(10000));
+}
+
+fn run_triangle_gen<T>(device: &cpal::Device, config: &cpal::StreamConfig, frequency: f32)
+where
+    T: cpal::Sample + cpal::SizedSample + cpal::FromSample<f32>,
+{
+    let mut sample_clock = 0f32;
+    // Produce a sinusoid of maximum amplitude.
+    let mut next_value = move || {
+        sample_clock = (sample_clock + 1.0) % SAMPLE_RATE;
+        4.0 * ((frequency / SAMPLE_RATE * sample_clock)
+            - ((frequency / SAMPLE_RATE * sample_clock) + 0.5)
+            .floor()).abs()
+            - 1.0
+    };
+
+    let channels = config.channels as usize;
+    let err_fn =
+        |err| eprintln!("an error occurred on the output audio stream: {}", err);
+    let stream = create_stream!(device, config, channels, err_fn, &mut next_value);
+    stream.play().unwrap();
+    // Keep the stream alive indefinitely to play sound
+    std::thread::sleep(std::time::Duration::from_millis(10000));
+}
+
+fn run_square_gen<T>(device: &cpal::Device, config: &cpal::StreamConfig, frequency: f32)
+where
+    T: cpal::Sample + cpal::SizedSample + cpal::FromSample<f32>,
+{
+    let mut sample_clock = 0f32;
+    // Produce a sinusoid of maximum amplitude.
+    let mut next_value = move || {
+        sample_clock = (sample_clock + 1.0) % SAMPLE_RATE;
+        if (sample_clock * frequency / SAMPLE_RATE) % 1.0 < 0.5 {
+            1.0
+        } else {
+            -1.0
+        }
+    };
+
+    let channels = config.channels as usize;
+    let err_fn =
+        |err| eprintln!("an error occurred on the output audio stream: {}", err);
+    let stream = create_stream!(device, config, channels, err_fn, &mut next_value);
+    stream.play().unwrap();
+    // Keep the stream alive indefinitely to play sound
+    std::thread::sleep(std::time::Duration::from_millis(10000));
+}
+
+fn run_saw_gen<T>(device: &cpal::Device, config: &cpal::StreamConfig, frequency: f32)
+where
+    T: cpal::Sample + cpal::SizedSample + cpal::FromSample<f32>,
+{
+    let mut sample_clock = 0f32;
+    // Produce a sinusoid of maximum amplitude.
+    let mut next_value = move || {
+        sample_clock = (sample_clock + 1.0) % SAMPLE_RATE;
+        2.0 * ((frequency / SAMPLE_RATE * sample_clock)
+            - ((frequency / SAMPLE_RATE * sample_clock) + 0.5)
+            .floor()).abs()
+            - 1.0
+    };
+
+    let channels = config.channels as usize;
+    let err_fn =
+        |err| eprintln!("an error occurred on the output audio stream: {}", err);
+    let stream = create_stream!(device, config, channels, err_fn, &mut next_value);
+    stream.play().unwrap();
+    // Keep the stream alive indefinitely to play sound
+    std::thread::sleep(std::time::Duration::from_millis(10000));
 }
