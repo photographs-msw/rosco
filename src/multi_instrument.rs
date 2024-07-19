6339b6aa199b9acb03a8325fea45bccc86db02d3
photographs-msw <rubles.canto0l@icloud.com>
2024-07-19 17:38:58 -0400

    Added validation helpers to multi-instrument. Fixed implementation of add_chord_to_channels().

--- src/multi_instrument.rs
+++ src/multi_instrument.rs
@@ -72,22 +72,50 @@ impl MultiInstrument {
     }
 
     pub(crate) fn add_note_to_channel(&mut self, channel_num: usize, note: Note) {
+        self.validate_channel_num(channel_num);
+
         self.channels[channel_num].sequence.add_note(note);
     }
 
     pub(crate) fn add_note_to_channels(&mut self, note: Note) {
+        self.validate_has_channels();
+
         for channel in &mut self.channels {
             channel.sequence.add_note(note);
         }
     }
 
+    pub(crate) fn add_chord_to_channels(&mut self, channel_nums: Vec<usize>, chord: Vec<Note>) {
+        for channel_num in &channel_nums {
+            self.validate_channel_num(*channel_num);
+        }
+        if channel_nums.len() != chord.len() {
+            panic!("Number of channels must match number of notes in chord");
+        }
+        let first_index: usize = self.channels[channel_nums[0]].sequence.get_index();
+        for channel_num in &channel_nums[1..] {
+            if self.channels[*channel_num].sequence.get_index() != first_index {
+                panic!("Channels must all be at the same index to add chord notes across \
+                        channel sequences");
+            }
+        }
+
+        for (channel_num, note) in channel_nums.iter().zip(chord) {
+            self.channels[*channel_num].sequence.add_note(note);
+        }
+    }
+
     pub(crate) fn set_volume_for_channels(&mut self, volume: f32) {
+        self.validate_has_channels();
+
         for channel in &mut self.channels {
             channel.volume = volume;
         }
     }
 
     pub(crate) fn set_volume_for_channel(&mut self, channel_num: usize, volume: f32) {
+        self.validate_channel_num(channel_num);
+
         self.channels[channel_num].volume = volume;
     }
 
@@ -106,5 +134,17 @@ impl MultiInstrument {
             notes.push(note);
         }
         notes
+    }
+
+    fn validate_channel_num(&self, channel_num: usize) {
+        if channel_num >= self.channels.len() {
+            panic!("Invalid channel number");
+        }
+    }
+
+    fn validate_has_channels(&self) {
+        if self.channels.len() == 0 {
+            panic!("No channels available");
+        }
     }
 }
