26ec58193e671bd6054792683f6f87fee9c389ff
photographs-msw <rubles.canto0l@icloud.com>
2024-08-03 23:21:52 -0400

    Modified to not copy notes on gen for multiple notes but rather collect and pass volume and frequency as float vectors

--- src/audio_gen.rs
+++ src/audio_gen.rs
@@ -14,38 +14,34 @@ pub(crate) fn gen_notes_audio(notes: &Vec<Note>,
                               track_waveforms: &Vec<Vec<u8>>) {
     let note_max_duration_ms=
         f32::max(1000.0, notes.iter().map(|note| note.duration_ms).sum());
-    gen_notes_impl::<f32>(notes.clone(), track_waveforms.clone(), note_max_duration_ms);
+    let volumes: Vec<f32> = notes.iter().map(|note| note.volume).collect();
+    let frequencies: Vec<f32> = notes.iter().map(|note| note.frequency).collect();
+    gen_notes_impl::<f32>(volumes, frequencies, track_waveforms.clone(), note_max_duration_ms);
 }
 
 /**
  * This macro generates a closure that will be used to generate the next value in the audio stream
- * It takes a vector of waveforms, volume, frequency, and sample clock as input and returns a closure
- * calls the oscillator::get_waveform_sample function for each waveform in the vector and sums the
- * results. Do this because we can't pass the waveforms vector into the closure directly, because
- * it used in a `move` callback, which means it will be run in another thread with an unknown
- * lifetime. This is a workaround to get the sum of calling all the waveforms into the closure.
  */
 macro_rules! generate_next_value {
     ($waveforms:expr, $volume:expr, $frequency:expr, $sample_clock:expr) => {
         move || {
             $waveforms.iter().map(|waveform| {
                 $volume * oscillator::get_note_sample(*waveform, $frequency, $sample_clock)
-            }).sum()
+            }).sum::<f32>()
         }
     };
 }
 
 /**
-* Same as above but for multiple notes, for each note it calls the oscillator::get_waveform_sample
-* with the associated set of waveforms for that note and sums the results.
+* Same as above but for multiple notes
 */
 macro_rules! generate_next_values {
-    ($notes:expr, $track_waveforms:expr, $sample_clock:expr) => {
+    ($volumes:expr, $frequencies:expr, $track_waveforms:expr, $sample_clock:expr) => {
         move || {
             let mut sample: f32 = 0.0;
-            for (i, note) in $notes.iter().enumerate() {
+            for i in 0..$volumes.len() {
                 sample += $track_waveforms[i].iter().map(|waveform| {
-                    note.volume * oscillator::get_note_sample(*waveform, note.frequency,
+                    $volumes[i] * oscillator::get_note_sample(*waveform, $frequencies[i],
                                                               $sample_clock)
                 }).sum::<f32>()
             }
@@ -62,8 +58,7 @@ where
     let host = cpal::default_host();
     let device = host.default_output_device().expect("No output device available");
     let config: cpal::StreamConfig = device.default_output_config().unwrap().into();
-    let mut sample_clock = 0f32;
-    sample_clock = (sample_clock + 1.0) % oscillator::SAMPLE_RATE;
+    let sample_clock = (0f32 + 1.0) % oscillator::SAMPLE_RATE;
 
     let mut next_value = generate_next_value!(waveforms, volume, frequency, sample_clock);
 
@@ -84,17 +79,18 @@ where
 }
 
 //noinspection DuplicatedCode
-fn gen_notes_impl<T>(notes: Vec<Note>, track_waveforms: Vec<Vec<u8>>, note_max_duration_ms: f32)
+fn gen_notes_impl<T>(volumes: Vec<f32>, frequencies: Vec<f32>, track_waveforms: Vec<Vec<u8>>,
+                     note_max_duration_ms: f32)
 where
     T: cpal::Sample + cpal::SizedSample + cpal::FromSample<f32>,
 {
     let host = cpal::default_host();
     let device = host.default_output_device().expect("No output device available");
     let config: cpal::StreamConfig = device.default_output_config().unwrap().into();
-    let mut sample_clock = 0f32;
-    sample_clock = (sample_clock + 1.0) % oscillator::SAMPLE_RATE;
+    let sample_clock = (0f32 + 1.0) % oscillator::SAMPLE_RATE;
 
-    let mut next_value = generate_next_values!(&notes, track_waveforms, sample_clock);
+    let mut next_value = generate_next_values!(volumes, frequencies, track_waveforms,
+                                                         sample_clock);
 
     let channels = config.channels as usize;
     let err_fn =
