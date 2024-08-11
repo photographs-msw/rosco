035c70406c58d71f3db1b047a0ed3919ce4b0df1
photographs-msw <rubles.canto0l@icloud.com>
2024-08-11 13:50:51 -0400

    first unit tests, for Note, testing is_playing and duration_position logic

--- src/note.rs
+++ src/note.rs
@@ -8,31 +8,39 @@ pub(crate) struct Note {
     pub(crate) frequency: f32,
     pub(crate) duration_ms: f32,
 
+    #[builder(default = "DEFAULT_VOLUME")]
+    pub(crate) volume: f32,
+
     #[builder(default = "INIT_START_TIME")]
     #[allow(dead_code)]
     pub(crate) start_time_ms: f32,
 
-    #[builder(public, setter(custom))]
+    #[builder(setter(custom))]
     #[allow(dead_code)]
     pub (crate) end_time_ms: f32,
 
-    #[builder(default = "DEFAULT_VOLUME")]
-    pub(crate) volume: f32,
+    #[builder(setter(custom))]
+    pub(crate) cur_playing_time_ms: Option<f32>,
+}
 
-    #[builder(setter(custom), default = "INIT_START_TIME")]
-    pub(crate) cur_playing_time_ms: f32,
+impl NoteBuilder {
+    pub(crate) fn end_time_ms(&mut self) -> &mut Self {
+        let start_time_ms = self.start_time_ms.unwrap();
+        let duration_ms = self.duration_ms.unwrap();
+        self.end_time_ms = Some(start_time_ms + duration_ms);
+        self
+    }
+
+    pub(crate) fn cur_playing_time_ms(&mut self, cur_playing_time_ms: Option<f32>) -> &mut Self {
+        self.cur_playing_time_ms = Some(cur_playing_time_ms.or(Some(INIT_START_TIME)));
+        self
+    }
 }
 
 #[allow(dead_code)]
 impl Note {
-    pub(crate) fn end_time_ms(&mut self) -> &mut Self {
-        self.end_time_ms = self.start_time_ms + self.duration_ms;
-        self
-    }
-
-    pub(crate) fn cur_playing_time_ms(&mut self, cur_playing_time_ms: f32) -> &mut Self {
-        self.cur_playing_time_ms = cur_playing_time_ms;
-        self
+    pub(crate) fn cur_playing_time_ms(&mut self, cur_playing_time_ms: f32) {
+        self.cur_playing_time_ms = Some(cur_playing_time_ms);
     }
 
     pub(crate) fn is_playing(&self, time_ms: f32) -> bool {
@@ -45,5 +53,74 @@ impl Note {
 
     pub(crate) fn is_after_playing(&self, time_ms: f32) -> bool {
         time_ms >= self.end_time_ms
+    }
+
+    pub(crate) fn duration_position(&self, cur_time_ms: f32) -> f32 {
+        (cur_time_ms - self.start_time_ms) / self.duration_ms
+    }
+}
+
+mod test_note {
+    #[cfg(test)]
+    mod test_note {
+        use crate::note::NoteBuilder;
+
+        #[test]
+        fn test_is_playing() {
+            let note = setup_note()
+                .start_time_ms(0.0)
+                .end_time_ms()
+                .cur_playing_time_ms(None)
+                .build().unwrap();
+
+            assert_eq!(note.is_playing(0.0), true);
+            assert_eq!(note.is_playing(500.0), true);
+            assert_eq!(note.is_playing(1000.0), false);
+        }
+
+        #[test]
+        fn test_is_before_playing() {
+            let note = setup_note()
+                .start_time_ms(0.01)
+                .end_time_ms()
+                .cur_playing_time_ms(None)
+                .build().unwrap();
+
+            assert_eq!(note.is_before_playing(0.0), true);
+            assert_eq!(note.is_before_playing(0.02), false);
+        }
+
+        #[test]
+        fn test_is_after_playing() {
+            let note = setup_note()
+                .start_time_ms(0.0)
+                .end_time_ms()
+                .cur_playing_time_ms(None)
+                .build().unwrap();
+
+            assert_eq!(note.is_after_playing(0.0), false);
+            assert_eq!(note.is_after_playing(500.0), false);
+            assert_eq!(note.is_after_playing(1000.0), true);
+        }
+
+        #[test]
+        fn test_duration_position() {
+            let note = setup_note()
+                .start_time_ms(0.0)
+                .end_time_ms()
+                .cur_playing_time_ms(None)
+                .build().unwrap();
+
+            assert_eq!(note.duration_position(0.0), 0.0);
+            assert_eq!(note.duration_position(500.0), 0.5);
+            assert_eq!(note.duration_position(1000.0), 1.0);
+        }
+
+        fn setup_note() -> NoteBuilder {
+            NoteBuilder::default()
+                .frequency(440.0)
+                .duration_ms(1000.0)
+                .volume(1.0).clone()
+        }
     }
 }
--- src/sequence_grid.rs
+++ src/sequence_grid.rs
@@ -9,21 +9,21 @@ pub(crate) struct SequenceGrid {
 
 #[allow(dead_code)]
 impl SequenceGrid {
-    pub(crate) fn to_sample_rate_index(&self) -> f32 {
+    pub(crate) fn get_sample_rate_index(&self) -> f32 {
         self.sample_clock_index % oscillator::SAMPLE_RATE
     }
 
-    pub(crate) fn to_current_time_ms(&self) -> f32 {
+    pub(crate) fn get_current_time_ms(&self) -> f32 {
         self.sample_clock_index / oscillator::SAMPLE_RATE
     }
 
     pub(crate) fn active_notes(&mut self) -> Vec<Note> {
-        let cur_time = self.to_current_time_ms();
+        let cur_time_ms = self.get_current_time_ms();
         let mut active_notes = Vec::new();
         for sequence in &mut self.sequences.iter_mut() {
             for note in sequence.iter_mut()  {
-                if note.is_playing(cur_time) {
-                    note.cur_playing_time_ms(cur_time);
+                if note.is_playing(cur_time_ms) {
+                    note.cur_playing_time_ms(cur_time_ms);
                     active_notes.push(note.clone());
                 }
             }
