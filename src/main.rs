1a1218c35a72e9e5d09beedb53021b0398983c46
photographs-msw <rubles.canto0l@icloud.com>
2024-08-13 18:22:50 -0400

    Add some organizing comments to main and fix warning

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,17 @@ use crate::note::{Note, NoteBuilder};
 use crate::track_grid::{TrackGrid, TrackGridBuilder};
 
 fn main() {
+    // Collect CLI Args
     let args = utils::get_cli_args();
     let waveforms_arg = args[0].clone();
     let frequency: f32 = args[1].parse().unwrap();
     let volume: f32 = args[2].parse().unwrap();
     let duration_ms: f32 = args[3].parse().unwrap();
 
+    // Test loading MIDI file and playing back using multi-track, polyphonic grid with one
+    // set of waveforms per track, notes per track, playing notes in windows of when they are active
+    // and coordinated concurrent playback where one thread prepares the next window to play
+    // and the other thread plays the current window
     let midi_tracks =
         midi::midi_file_to_tracks("/Users/markweiss/Downloads/test.mid");
     let mut track_grid: TrackGrid = TrackGridBuilder::default()
@@ -34,22 +39,12 @@ fn main() {
         .sample_clock_index(0.0)
         .build().unwrap();
 
-    // TODO MAKE THIS MULTITHREADED
-    /*
-        let (tx, rx) = mpsc::channel();
-
-    thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
-    });
-    */
     let (tx, rx) = mpsc::channel();
     thread::spawn(move || {
         // TODO IMPLEMENT ITERATOR PATTERN FOR NOTES_WINDOW TO GET RID OF WHILE LOOP
         let mut notes_window = track_grid.next_notes_window();
-        let window_duration_ms = notes_window.window_duration_ms();
         while !notes_window.is_empty() {
-            tx.send(notes_window);
+            tx.send(notes_window).unwrap();
             notes_window = track_grid.next_notes_window();
         }
     });
@@ -60,84 +55,89 @@ fn main() {
                              window_duration_ms as u64);
     }
 
-    // let midi_tracks_2 =
-    //     midi::midi_file_to_tracks("/Users/markweiss/Downloads/test.mid");
-    // let waveforms_1 = oscillator::get_waveforms(&waveforms_arg);
-    // let waveform_2 = oscillator::get_waveforms(&waveforms_arg);
-    // let midi_track_waveforms = vec![waveforms_1, waveform_2];
-    // let num_tracks = midi_track_waveforms.len();
-    // let mut midi_multi_instrument = MultiInstrumentBuilder::default()
-    //     .track_waveforms(midi_track_waveforms)
-    //     .num_tracks(num_tracks)
-    //     .add_tracks(midi_tracks_2)
-    //     .build().unwrap();
-    // midi_multi_instrument.loop_once();
-    //
-    // let waveforms_3 = oscillator::get_waveforms(&waveforms_arg);
-    // let waveform_4 = oscillator::get_waveforms(&waveforms_arg);
-    // let track_waveforms = vec![waveforms_3, waveform_4];
-    // let num_tracks = track_waveforms.len();
-    // let mut multi_instrument = MultiInstrumentBuilder::default()
-    //     .track_waveforms(track_waveforms)
-    //     .num_tracks(num_tracks)
-    //     .tracks()
-    //     .build().unwrap();
-    // // builder with default volume
-    // let note_1: Note = NoteBuilder::default()
-    //     .frequency(frequency)
-    //     .start_time_ms(0.0)
-    //     .duration_ms(duration_ms)
-    //     .end_time_ms()
-    //     .build().unwrap();
-    // let note_2: Note = NoteBuilder::default()
-    //     .frequency(frequency)
-    //     .volume(volume * 0.75)
-    //     .start_time_ms(duration_ms)
-    //     .duration_ms(duration_ms)
-    //     .end_time_ms()
-    //     .build().unwrap();
-    // multi_instrument.add_note_to_track(0, note_1 );
-    // multi_instrument.add_note_to_track(1, note_2);
-    // multi_instrument.add_note_to_track(0, note_2 );
-    // multi_instrument.add_note_to_track(1, note_1);
-    // multi_instrument.add_note_to_tracks(note_1);
-    // multi_instrument.play_track_notes_and_advance();
-    // multi_instrument.set_volume_for_track(0, 0.25);
-    // multi_instrument.play_track_notes();
-    // multi_instrument.set_volume_for_tracks(0.75);
-    // multi_instrument.loop_once();
-    // multi_instrument.loop_n(2);
-    // multi_instrument.play_notes_direct(vec![note_1, note_2]);
-    //
-    // // override default builder volume of 1.0
-    // let instrument_volume: f32 = 0.9;
-    // let mut instrument = InstrumentBuilder::default()
-    //     .waveforms(oscillator::get_waveforms(&waveforms_arg))
-    //     .volume(instrument_volume)
-    //     .track()
-    //     .build().unwrap();
-    // let note_3: Note = NoteBuilder::default()
-    //     .frequency(frequency)
-    //     .start_time_ms(0.0)
-    //     .duration_ms(duration_ms)
-    //     .end_time_ms()
-    //     .build().unwrap();
-    // let note_4: Note = NoteBuilder::default()
-    //     .frequency(frequency)
-    //     .volume(volume * 0.75)
-    //     .start_time_ms(duration_ms)
-    //     .duration_ms(duration_ms)
-    //     .end_time_ms()
-    //     .build().unwrap();
-    // instrument.add_note(note_3);
-    // instrument.add_note(note_4);
-    // instrument.play_note_and_advance();
-    // instrument.set_volume(0.25);
-    // instrument.play_note();
-    // instrument.set_volume(0.75);
-    // instrument.loop_once();
-    // instrument.loop_n(2);
-    // instrument.reset();
-    // instrument.play_note();
-    // instrument.play_note_direct(&note_3);
+    // Setup MultiInstrument and Instrument
+    let midi_tracks_2 =
+        midi::midi_file_to_tracks("/Users/markweiss/Downloads/test.mid");
+    let waveforms_1 = oscillator::get_waveforms(&waveforms_arg);
+    let waveform_2 = oscillator::get_waveforms(&waveforms_arg);
+    let midi_track_waveforms = vec![waveforms_1, waveform_2];
+    let num_tracks = midi_track_waveforms.len();
+    let mut midi_multi_instrument = MultiInstrumentBuilder::default()
+        .track_waveforms(midi_track_waveforms)
+        .num_tracks(num_tracks)
+        .add_tracks(midi_tracks_2)
+        .build().unwrap();
+    midi_multi_instrument.loop_once();
+
+    let waveforms_3 = oscillator::get_waveforms(&waveforms_arg);
+    let waveform_4 = oscillator::get_waveforms(&waveforms_arg);
+    let track_waveforms = vec![waveforms_3, waveform_4];
+    let num_tracks = track_waveforms.len();
+    let mut multi_instrument = MultiInstrumentBuilder::default()
+        .track_waveforms(track_waveforms)
+        .num_tracks(num_tracks)
+        .tracks()
+        .build().unwrap();
+    // builder with default volume
+    let note_1: Note = NoteBuilder::default()
+        .frequency(frequency)
+        .start_time_ms(0.0)
+        .duration_ms(duration_ms)
+        .end_time_ms()
+        .build().unwrap();
+    let note_2: Note = NoteBuilder::default()
+        .frequency(frequency)
+        .volume(volume * 0.75)
+        .start_time_ms(duration_ms)
+        .duration_ms(duration_ms)
+        .end_time_ms()
+        .build().unwrap();
+
+    // Test MultiInstrument, primitive concurrent playback that simply gets the next note to play
+    // from each track
+    multi_instrument.add_note_to_track(0, note_1 );
+    multi_instrument.add_note_to_track(1, note_2);
+    multi_instrument.add_note_to_track(0, note_2 );
+    multi_instrument.add_note_to_track(1, note_1);
+    multi_instrument.add_note_to_tracks(note_1);
+    multi_instrument.play_track_notes_and_advance();
+    multi_instrument.set_volume_for_track(0, 0.25);
+    multi_instrument.play_track_notes();
+    multi_instrument.set_volume_for_tracks(0.75);
+    multi_instrument.loop_once();
+    multi_instrument.loop_n(2);
+    multi_instrument.play_notes_direct(vec![note_1, note_2]);
+
+    // Test single Instrument
+    // override default builder volume of 1.0
+    let instrument_volume: f32 = 0.9;
+    let mut instrument = InstrumentBuilder::default()
+        .waveforms(oscillator::get_waveforms(&waveforms_arg))
+        .volume(instrument_volume)
+        .track()
+        .build().unwrap();
+    let note_3: Note = NoteBuilder::default()
+        .frequency(frequency)
+        .start_time_ms(0.0)
+        .duration_ms(duration_ms)
+        .end_time_ms()
+        .build().unwrap();
+    let note_4: Note = NoteBuilder::default()
+        .frequency(frequency)
+        .volume(volume * 0.75)
+        .start_time_ms(duration_ms)
+        .duration_ms(duration_ms)
+        .end_time_ms()
+        .build().unwrap();
+    instrument.add_note(note_3);
+    instrument.add_note(note_4);
+    instrument.play_note_and_advance();
+    instrument.set_volume(0.25);
+    instrument.play_note();
+    instrument.set_volume(0.75);
+    instrument.loop_once();
+    instrument.loop_n(2);
+    instrument.reset();
+    instrument.play_note();
+    instrument.play_note_direct(&note_3);
 }
