5c0d59dda0583f0b656806183dc0f91bbda41fff
photographs-msw <rubles.canto0l@icloud.com>
2024-08-24 02:59:27 -0400

    implemented next notes window on the sequence with first passing test which actually tests end time cut off by start of next note

--- src/note_sequence.rs
+++ src/note_sequence.rs
@@ -1,8 +1,9 @@
 use derive_builder::Builder;
 use float_eq::{float_eq, float_ne};
+use once_cell::race::OnceNonZeroUsize;
 
 use crate::constants;
-use crate::note::Note;
+use crate::note::{Note, NoteBuilder};
 
 #[allow(dead_code)]
 static INIT_START_TIME: f32 = 0.0;
@@ -23,7 +24,13 @@ pub(crate) struct NoteSequence {
     sequence: Vec<Vec<Note>>,
     
     #[builder(default = "0")]
-    index: usize
+    index: usize,
+
+    #[builder(default = "0")]
+    next_notes_index: usize,
+
+    #[builder(default = "0.0")]
+    next_notes_time_ms: f32,
 }
 
 #[allow(dead_code)]
@@ -32,11 +39,13 @@ impl NoteSequence {
     // Manage Notes
     pub(crate) fn append_notes(&mut self, notes: Vec<Note>) {
         self.validate_notes_to_add(&notes);
-        
+
+        if self.index == 0 {
+            self.sequence.push(notes);
         // Maintain the invariant that all notes with the same start_time are grouped in one
         // note sequence at one index, so if these notes have the same start_time as current
         // notes in last position, add these to that position
-        if float_eq!(self.get_notes_start_time(self.index), notes[0].start_time_ms,
+        } else if float_eq!(self.notes_start_time(self.index), notes[0].start_time_ms,
                      rmax <= constants::FLOAT_EQ_TOLERANCE) {
             self.sequence[self.index].append(&mut notes.clone());
         } else {
@@ -59,13 +68,13 @@ impl NoteSequence {
         let notes_start_time_ms = notes[0].start_time_ms;
         let mut inserted = false;
         while insert_position < self.sequence.len() {
-            if float_eq!(self.get_notes_start_time(insert_position), notes_start_time_ms,
+            if float_eq!(self.notes_start_time(insert_position), notes_start_time_ms,
                          rmax <= constants::FLOAT_EQ_TOLERANCE) {
                 self.sequence[insert_position].append(&mut notes.clone());
                 inserted = true;
                 break;
             }
-            if self.get_notes_start_time(insert_position) > notes_start_time_ms {
+            if self.notes_start_time(insert_position) > notes_start_time_ms {
                 // Move all notes from this position until self.index one position forward
                 self.sequence.insert(insert_position, notes.clone());
                 self.index += 1;
@@ -87,7 +96,6 @@ impl NoteSequence {
     }
 
     pub(crate) fn append_note(&mut self, note: Note) {
-        self.validate_note_to_add(&note);
         self.append_notes(vec![note]);
     }
 
@@ -95,13 +103,77 @@ impl NoteSequence {
         self.validate_note_to_add(&note);
         self.insert_note_helper(&note);
     }
+    
+    pub(crate) fn get_next_notes_window(&mut self, notes_time_ms: f32) -> Vec<Note> {
+        let mut window_notes = Vec::new();
+        let next_notes_start_time_ms = self.notes_start_time(self.next_notes_index);
+        if notes_time_ms < next_notes_start_time_ms {
+            // emit a rest note
+            window_notes.push(
+                NoteBuilder::default()
+                    .frequency(440.0)
+                    .volume(1.0)
+                    .start_time_ms(notes_time_ms)
+                    .duration_ms(next_notes_start_time_ms - notes_time_ms)
+                    .end_time_ms()
+                    .build().unwrap()
+            );
+            
+            self.next_notes_time_ms = notes_time_ms;
+        } else if float_eq!(notes_time_ms, next_notes_start_time_ms,
+                            rmax <= constants::FLOAT_EQ_TOLERANCE) {
+            let end_time_ms = self.get_end_time_ms();
+            window_notes.append(
+                &mut self.sequence[self.next_notes_index].iter()
+                    .map(|note| {
+                        let mut new_note = note.clone();
+                        new_note.duration_ms = end_time_ms - new_note.start_time_ms;
+                        new_note
+                    }).collect()
+            );
+            
+            self.next_notes_time_ms = end_time_ms;
+        } else if notes_time_ms > next_notes_start_time_ms {
+            let end_time_ms = self.get_end_time_ms();
+            window_notes.append(
+                &mut self.sequence[self.next_notes_index].iter()
+                    .map(|note| {
+                        let mut new_note = note.clone();
+                        new_note.duration_ms = end_time_ms - new_note.start_time_ms;
+                        new_note
+                    }).collect()
+            );
+            
+            self.next_notes_time_ms = end_time_ms;
+        }
+        window_notes
+    }
+
+    fn get_end_time_ms(&self) -> f32 {
+        let mut end_time_ms = 0.0;
+        self.sequence[self.index].iter().for_each(|note| {
+            if note.end_time_ms > end_time_ms {
+                end_time_ms = note.end_time_ms;
+            }
+        });
+        // If next_notes isn't on the last notes, lookahead one position to see if the start
+        // time of the next next notes is < the max end time of the notes at current next notes
+        if self.next_notes_index <= self.index {
+            let next_next_notes_start_time_ms =
+                self.notes_start_time(self.next_notes_index + 1);
+            if next_next_notes_start_time_ms < end_time_ms {
+                end_time_ms = next_next_notes_start_time_ms;
+            }
+        }
+        end_time_ms
+    }
 
     pub(crate) fn max_start_time(&self) -> f32 {
         if self.sequence.is_empty() {
             return 0.0;
         }
         // Because notes are sorted by start time in append(), the last note has the max start time
-        self.sequence[self.sequence.len() - 1][0].start_time_ms
+        self.sequence[self.index][0].start_time_ms
     }
 
     // deprecated because makes no sense once we store a vector at each position
@@ -188,11 +260,15 @@ impl NoteSequence {
         self.sequence.len()
     }
     
-    fn get_notes_start_time(&self, index: usize) -> f32 {
+    fn notes_start_time(&self, index: usize) -> f32 {
         if index >= self.sequence.len() {
             panic!("Index out of bounds");
         }
         self.sequence[index][0].start_time_ms
+    }
+
+    fn next_notes_start_time(&self) -> f32 {
+        self.notes_start_time(self.index) 
     }
 
     fn validate_notes_to_add(&self, notes: &Vec<Note>) {
@@ -221,13 +297,13 @@ impl NoteSequence {
         let notes_start_time_ms = note.start_time_ms;
         let mut inserted = false;
         while insert_position < self.sequence.len() {
-            if float_eq!(self.get_notes_start_time(insert_position), notes_start_time_ms,
+            if float_eq!(self.notes_start_time(insert_position), notes_start_time_ms,
                          rmax <= constants::FLOAT_EQ_TOLERANCE) {
                 self.sequence[insert_position].push(note.clone());
                 inserted = true;
                 break;
             }
-            if self.get_notes_start_time(insert_position) > notes_start_time_ms {
+            if self.notes_start_time(insert_position) > notes_start_time_ms {
                 // Move all notes from this position until self.index one position forward
                 self.sequence.insert(insert_position, vec![note.clone()]);
                 self.index += 1;
@@ -246,6 +322,8 @@ impl NoteSequence {
 
 #[cfg(test)]
 mod test_note_sequence {
+    use float_eq::assert_float_eq;
+    use crate::constants;
     use crate::note::NoteBuilder;
     use crate::note_sequence::NoteSequenceBuilder;
 
@@ -403,6 +481,43 @@ mod test_note_sequence {
             .build().unwrap();
 
         sequence.insert_note(note_1);
+    }
+    
+    // TODO TEST APPEND SECOND NOTE ON SAME TIME AS AN EXISTING NOTE
+    
+    #[test]
+    fn test_get_next_notes_window() {
+        let note_1 = setup_note()
+            .start_time_ms(0.0)
+            .end_time_ms()
+            .build().unwrap();
+        let note_2 = setup_note()
+            .start_time_ms(500.0)
+            .end_time_ms()
+            .build().unwrap();
+        let note_3 = setup_note()
+            .start_time_ms(1000.0)
+            .end_time_ms()
+            .build().unwrap();
+        let note_4 = setup_note()
+            .start_time_ms(1000.0)
+            .end_time_ms()
+            .build().unwrap();
+        let note_5 = setup_note()
+            .start_time_ms(1500.0)
+            .end_time_ms()
+            .build().unwrap();
+        let mut sequence = NoteSequenceBuilder::default().build().unwrap();
+        
+        sequence.append_note(note_1.clone());
+        sequence.append_note(note_2.clone());
+        sequence.append_notes(vec![note_3.clone(), note_4.clone()]);
+        sequence.append_note(note_5.clone());
+        
+        let notes_window = sequence.get_next_notes_window(0.0);
+        assert_eq!(notes_window.len(), 1);
+        assert_eq!(sequence.get_index(), 0);
+        assert_float_eq!(notes_window[0].duration_ms, 500.0, rmax <= constants::FLOAT_EQ_TOLERANCE);
     }
     
     fn setup_note() -> NoteBuilder {
