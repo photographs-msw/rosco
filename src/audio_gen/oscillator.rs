348bee7dcd85f24f9b551f69e89a52c92a44e502
photographs-msw <rubles.canto0l@icloud.com>
2024-10-06 04:14:07 -0400

    playing gen and sample cleanly

--- src/audio_gen/oscillator.rs
+++ src/audio_gen/oscillator.rs
@@ -95,7 +95,7 @@ pub(crate) fn get_waveforms(waveform_arg: &str) -> Vec<Waveform> {
 }
 
 pub(crate) fn get_sample(table: &Vec<f32>, frequency: f32, sample_count: u64) -> f32 {
-    table[(frequency / 100.0).round() as usize * sample_count as usize % NUM_TABLE_SAMPLES]
+    table[(frequency / 200.0).round() as usize * sample_count as usize % NUM_TABLE_SAMPLES]
 }
 
 pub(crate) fn get_gaussian_noise_sample() -> f32 {
