d8308195fec5c6f642de9de8590392d03b6d0d4d
photographs-msw <rubles.canto0l@icloud.com>
2025-06-21 14:08:12 -0400

    One more missed merge fix. Rebased onto main and DSL is working, and so is FixedTimeNoteSequence. Amazing.

--- src/composition/mod.rs
+++ src/composition/mod.rs
@@ -1,7 +1,4 @@
 pub mod comp_utils;
 pub mod computer_punk_001;
-<<<<<<< HEAD
 pub mod computer_punk_003;
-=======
 pub mod dsl_1;
->>>>>>> eb9b2dd (DSL composition parsing and running but no sound)
