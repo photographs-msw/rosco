147d486bfafef006744aafea41c735e890f0844a
photographs-msw <rubles.canto0l@icloud.com>
2025-06-22 17:47:01 -0400

    Tweak the dsl composition

--- src/compositions/dsl_1.rs
+++ src/compositions/dsl_1.rs
@@ -5,32 +5,36 @@ pub(crate) fn play() {
     println!("playing dsl 1");
 
     let input = r#"
-    let env1 = a 0.2,0.8 d 0.3,0.6 s 0.8,0.9 r 1.0,0.0
-    let delay1 = delay mix 0.9 decay 0.1 interval_ms 100.0 duration_ms 50.0 num_repeats 3 num_predelay_samples 10 num_concurrent_delays 2 
-    let flanger1 = flanger window_size 15 mix 0.5
+    let env1 = a 0.2,0.8 d 0.3,0.6 s 0.8,0.7 r 1.0,0.0
+    let delay1 = delay mix 0.7 decay 0.5 interval_ms 50.0 duration_ms 30.0 num_repeats 8 num_predelay_samples 10 num_concurrent_delays 2 
+    let flanger1 = flanger window_size 25 mix 0.5
 
-    FixedTimeNoteSequence dur Half tempo 60 num_steps 16
+    FixedTimeNoteSequence dur Half tempo 40 num_steps 16
     $env1
     $delay1
     $flanger1
 
     osc:sine:440.0:0.9:0
     osc:triangle:440.0:0.3:0
+    osc:triangle:440.0:0.3:2
 
     samp:/Users/markweiss/Downloads/punk_computer/003/piano_note_1_clipped.wav:0.005:2
     
     osc:square:880.0:9.5:4
     osc:triangle:880.0:3.5:4
+    osc:triangle:880.0:3.5:6
 
     samp:/Users/markweiss/Downloads/punk_computer/003/piano_note_1_clipped.wav:0.005:6
     
     osc:sine:440.0:0.9:8
     osc:triangle:440.0:0.3:8
+    osc:triangle:440.0:0.3:10
 
     samp:/Users/markweiss/Downloads/punk_computer/003/piano_note_1_clipped.wav:0.005:10
     
     osc:square:880.0:9.5:12
     osc:triangle:880.0:3.5:12
+    osc:triangle:880.0:3.5:14
     
     samp:/Users/markweiss/Downloads/punk_computer/003/piano_note_1_clipped.wav:0.005:14
 "#;
